82310af73497a016dac332c1e7a3fa9de394d3a3
faludi ors <faludiors@gmail.com>
2019-01-23 16:50:19 +0200

    Made requested changes

--- src/atoms/Loader/spec.js
+++ src/atoms/Loader/spec.js
@@ -2,14 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Loader from 'atoms/Loader';
 
-// describe('The loader atom', () => {
-//   it('Should render the container, blimps and spinner elements', () => {
-//     const wrapper = shallow(<Loader />);
-//     const containerDiv = wrapper.children();
-//     expect(containerDiv.length).toBe(5)
-//   });
-// });
-
 describe('The loader atom', () => {
   it('Should render the container, blimps and spinner elements', () => {
     const wrapper = shallow(<Loader />);
